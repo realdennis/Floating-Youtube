baa48541c1c7d2c5a752fba23f806d234fb2f6aa
realdennis <evolon688@gmail.com>
2019-01-26 04:57:36 +0800

    using classlist add / remove 2 good methods

--- src/lib/switcher.js
+++ src/lib/switcher.js
@@ -1,11 +1,10 @@
 const switcher = () => {
   const inViewPort =
-    document.querySelector('ytd-player').getBoundingClientRect().bottom >
-    56;
+    document.querySelector('ytd-player').getBoundingClientRect().bottom > 56;
   const video = document.querySelector('video');
   if (!inViewPort && !video.className.includes('float-video'))
-    video.className += ' float-video';
+    video.classList.add('float-video');
   if (inViewPort && video.className.includes('float-video'))
-    video.className = video.className.replace(' float-video', '');
+    video.classList.remove('float-video');
 };
 export default switcher;
