f337bc2c43850be9a3d373e0263b98a3a340aca9
realdennis <evolon688@gmail.com>
2019-01-29 05:04:21 +0800

    change the mutex position and settimeout workaround

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import floatStyle from './lib/floatStyle';
 import switcher from './lib/switcher';
 const float_video = () => {
   if (window._floatIsOpen === true) return;
-  window._floatIsOpen = true;
   document.body.append(floatStyle);
   document.querySelector('div#movie_player').style.zIndex = 2;
   window.addEventListener('scroll', throttle(switcher, 100));
+  // Actually done without exception
+  window._floatIsOpen = true;
 };
 location.href.includes('watch')
   ? float_video()
   : document.addEventListener('yt-navigate-finish', e => {
       if (e.target.baseURI.includes('watch')) {
-        float_video();
+        // workaround
+        setTimeout(float_video, 0);
       }
     });
